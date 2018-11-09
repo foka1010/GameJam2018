0d41452e174bc7ceb62e1fcface8289565d5fe60
Bugarick <galdikpeter@gmail.com>
2018-11-09 22:19:15 +0100

    Basic gameplay

---
+++ Assets/LevelSpawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSpawner : MonoBehaviour
+{
+    public GameObject groundTile;
+    public float segmentLenght;
+
+    public int levelLenght = 20;
+
+    void Start()
+    {
+        LayoutGround();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void LayoutGround()
+    {
+
+        for(int i = 0; i < levelLenght; i++)
+        {
+            Instantiate(groundTile, new Vector3(segmentLenght * i, 0, 0), Quaternion.identity);
+        }
+    }
+}
---
+++ Assets/MartinController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MartinController : MonoBehaviour
+{
+    public float speed = 5f;
+    public float upAndDownSpeed = 5f;
+    public float upAndDownRadius = 5f;
+
+    public GameObject trap;
+
+    float height;
+
+    void Start()
+    {
+        height = transform.position.y;
+        StartCoroutine(LeaveTraps());
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        transform.position = new Vector3(transform.position.x + speed * Time.deltaTime ,height + Mathf.Sin(Time.time*upAndDownSpeed) * upAndDownRadius, 0.0f);
+    }
+
+
+    IEnumerator LeaveTraps()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(2f);
+            Instantiate(trap, gameObject.transform.position, Quaternion.identity);
+        }
+    }
+}
